2dadcaa4eeb0d89145415997548d237448007a98
Lucas de Bona Sartor <lucas.bsartor@pagar.me>
2022-09-21 16:08:02 -0300

    Added Employees Resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { CustomersModule } from './customers/customers.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [PrismaModule, CustomersModule],
+  imports: [PrismaModule, CustomersModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
