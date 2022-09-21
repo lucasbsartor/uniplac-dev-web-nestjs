eea51cdfe37c26a6a398c4a59302f6be595cd63c
Lucas de Bona Sartor <lucas.bsartor@pagar.me>
2022-09-21 16:08:35 -0300

    Added Products Resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { CustomersModule } from './customers/customers.module';
 import { EmployeesModule } from './employees/employees.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [PrismaModule, CustomersModule, EmployeesModule],
+  imports: [PrismaModule, CustomersModule, EmployeesModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
