3c761d5e3443c187c122b7365aae34f2be14b132
Lucas de Bona Sartor <lucas.bsartor@pagar.me>
2022-09-21 16:07:31 -0300

    Added Customers Resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
